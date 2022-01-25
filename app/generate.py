2fad603dce2ab12b24428bade51b024a35e4a90c
ranon rat <66473662+ranon-rat@users.noreply.github.com>
2022-01-25 03:32:17 -0600

    just for avoid deletion of the directories

--- app/generate.py
+++ app/generate.py
@@ -76,4 +76,53 @@ def concat_all(text: str, font_size: int, name: str, width: int, height: int, fo
          "-c", "copy", "video/"+name+".mp4"])
     clear_directory("videos")
 
+text="""
+Okay, I pull up, hop out at the after party
+You and all your friends, yeah, they love to get naughty
+Sippin' on that Henn', I know you love that Bacardi (Sonny Digital)
+1942, I take you back in that 'Rari
+Okay, I pull up, hop out at the after party
+You and all your friends, yeah, they love to get naughty
+Sippin' on that Henn', I know you love that Bacardi
+1942, I take you back in that 'Rari
+Switchin' lanes and I'm seein' lights, you know I watch the curb
+Smokin' weed, you know I'm gettin' high, you know it calm my nerves
+Trappin' hard, pumpin' non-stop, the bag I had to earn
+You can try, but you might fail again, you know you never learn
+Came in with a bottle, I was trippin'
+And I took a couple shots and now I'm dizzy
+It got me burning up, burning up
+Insides burning up, burning up (Yeah)
+Okay, I pull up, hop out at the after party
+You and all your friends, yeah, they love to get naughty
+Sippin' on that Henn', I know you love that Bacardi
+1942, I take you back in that 'Rari
+Okay, I pull up, hop out at the after party
+You and all your friends, yeah, they love to get naughty
+Sippin' on that Henn', I know you love that Bacardi
+1942, I take you back in that 'Rari
+Uh, poppin' meds (Uh), out in the meadows (Yeah)
+She like a little red (Uh), in her stilettos (Yeah)
+Anytime you live (Uh), out in the ghetto (Yeah)
+You try to duck the feds (Uh), they need to let go
+Came in with a bottle, I was trippin'
+And I took a couple shots, it got me dizzy
+It got me burning up, burning up
+Insides burning up, burning up (Yeah)
+Okay, I pull up, hop out at the after party
+You and all your friends, yeah, they love to get naughty
+Sippin' on that Henn', I know you love that Bacardi
+1942, I take you back in that 'Rari
+Okay, I pull up, hop out at the after party
+You and all your friends, yeah, they love to get naughty
+Sippin' on that Henn', I know you love that Bacardi
+1942, I take you back in that 'Rari
+Okay, I pull up (Okay, I pull up)
+Okay, I pull up (Okay, I pull up)
+Okay, I pull up (Okay, I pull up), hop out at the after party
+Okay, I pull up (Okay, I pull up), hop out at the after party
+Okay, I pull up (Okay, I pull up), hop out at the after party
+Okay, I pull up (Okay, I pull up), hop out at the after party
+"""
 
+concat_all(text,10,"okayIPullUp",400,400)
